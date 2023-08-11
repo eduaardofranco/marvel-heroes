5b2a2f4c3889c9dd4c5ce9ad2bc66d64383c2ef9
eduaardofranco <eduaardo.franco@gmail.com>
2023-08-11 12:00:44 +0100

    add heroe modal functionalities

--- js/Heores.js
+++ js/Heores.js
@@ -9,11 +9,12 @@ export class MarvelApi {
         .then(res => res.data.results)
         .then(results => {
             // Assuming results is an array containing objects
-            return results.map(({ id, name, description, thumbnail }) => ({
+            return results.map(({ id, name, description, thumbnail, series }) => ({
               id,
               name,
               description,
-              thumbnail
+              thumbnail,
+              series
             }));
           })
         .catch((err) => {
@@ -26,6 +27,7 @@ export class Heores {
 
         this.root = document.getElementById(root)
         this.container = document.querySelector('.character-container')
+        this.containerModal = document.querySelector('.character-modal')
     }
 }
 
@@ -41,12 +43,24 @@ export class HeroesView extends Heores {
         //load the firsts heroes
         this.loadMore()
 
+        this.character = document.querySelector('.character a')
         this.loadMoreButton = document.querySelector('.load-more')
         this.searchInputText = document.getElementById('search')
         this.searchNotFound = document.querySelector('.search-result')
         this.searchNotFoundValue = document.querySelector('.search-result span')
         this.searchButton = document.getElementById('search-input')
+        this.closeModal = document.querySelector('.heroe-stats-container .close-modal')
+        this.characterModal = document.querySelector('.character-modal')
 
+
+        //event delegation
+        //attach the addeventlistener to the parent and when the character html is created add to him
+        this.container.addEventListener('click', (event)=> {
+            event.preventDefault()
+            const heroCard = event.target.closest('.character a').dataset.id
+            this.updateHeroeDetails(heroCard)
+            // console.log(heroCard)
+        })
         
         // Use an arrow function to preserve the context
         this.loadMoreButton.addEventListener('click', (event) => {
@@ -64,7 +78,14 @@ export class HeroesView extends Heores {
             const inputValue = event.target.value;
             debouncedSearch(inputValue); // Call the debounced function and pass the input value
           });
-        
+        // close modal func
+        //event delegation because the close button was not created yet
+        this.characterModal.addEventListener('click', function(event) {
+            event.preventDefault()
+            if(event.target.tagName === 'A') {
+                document.querySelector('.heroe-stats-container').remove()
+            }
+        })
     }
     async loadApi() {
         //get the response from marvel fetch class and assign to heroes variable
@@ -143,6 +164,7 @@ export class HeroesView extends Heores {
 
         heroesToDisplay.forEach((heroeItem) => {
             const heroe = this.createHeroe()
+            heroe.querySelector('.character a').dataset.id = heroeItem.id
             heroe.querySelector('.character img').src = heroeItem.thumbnail.path+'/portrait_uncanny.'+heroeItem.thumbnail.extension
             heroe.querySelector('.character img').alt = heroeItem.name +' photo'
             heroe.querySelector('.character h3').textContent = heroeItem.name
@@ -162,7 +184,7 @@ export class HeroesView extends Heores {
 
         character.innerHTML = 
         `
-            <a href="">
+            <a href="" data-set="">
                 <figure>
                     <img src="" alt="">
                 </figure>
@@ -173,6 +195,54 @@ export class HeroesView extends Heores {
             </a>
         `
         return character
+    }
+
+
+    async updateHeroeDetails(heroeId) {
+
+            const heroes = await this.loadApi()
+            const heroDetail = heroes.find((heroe) => heroe.id == heroeId)
+            console.log(heroDetail)
+
+            const heroe = this.createHeroDetail()
+            heroe.querySelector('.heroe-stats-container h2').innerText = heroDetail.name
+            heroe.querySelector('.heroe-stats-container img').src = heroDetail.thumbnail.path+'/portrait_uncanny.'+heroDetail.thumbnail.extension
+            heroe.querySelector('.heroe-stats-container img').alt = heroDetail.name +' photo'
+            heroe.querySelector('.heroe-stats-container .details p').innerText = heroDetail.description
+            heroDetail.series.items.forEach((serie) => {
+                const p = document.createElement('p')
+                p.innerText = serie.name
+                heroe.querySelector('.heroe-stats-container .series').append(p)
+
+            })
+
+            this.containerModal.append(heroe)
+    }
+
+    createHeroDetail() {
+
+        const detailContainer = document.createElement('div')
+        detailContainer.classList.add('heroe-stats-container')
+
+        detailContainer.innerHTML = 
+        `
+        <a href="" class="close">
+                <?xml version="1.0" ?><svg data-name="Livello 1" id="Livello_1" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg"><title/><path d="M64,0a64,64,0,1,0,64,64A64.07,64.07,0,0,0,64,0Zm0,122a58,58,0,1,1,58-58A58.07,58.07,0,0,1,64,122Z"/><path d="M92.12,35.79a3,3,0,0,0-4.24,0L64,59.75l-23.87-24A3,3,0,0,0,35.88,40L59.76,64,35.88,88a3,3,0,0,0,4.25,4.24L64,68.25l23.88,24A3,3,0,0,0,92.13,88L68.24,64,92.13,40A3,3,0,0,0,92.12,35.79Z"/></svg>
+            </a>
+            <div class="container">
+                <img src="assets/deadpool.png" alt="Deadpool">
+                <div class="details">
+                    <h2>Spider Man</h2>
+                    <p class="description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam sapiente, quo eligendi molestias laborum sed. Aperiam, nostrum pariatur fuga, itaque quae corporis ipsam non obcaecati, iusto minima hic similique earum!
+                    </p>
+                    <div class="series">
+                        <h4>Series</h4>
+                    </div>
+                </div>
+            </div>
+        `
+        return detailContainer
     }
 
 }
